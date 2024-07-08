19d4e74165b219ad43c22db7b5068daf5d13f5cc
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-07 22:05:50 -0500

    Making files more readable - removing comments

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
 const inquirer = require('inquirer')
-// const colors = require('colors')
 const fs = require('fs')
-// const shapes = require('./shapeExamples')
 const { Shape, Circle, Square, Triangle } = require('./lib/shape')
 
-
-
-// class SVG {
-//     constructor(shape, text, textColor) {
-//       this.shape = shape
-//       this.text = text
-//       this.textColor = textColor
-//     }
-//     render() {
-//       return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shape}${this.text}${this.textColor}</svg>`;
-//     }
-//     setText(text, color) {
-//         this.text = text
-//         this.color = color
-  
-//     }
-//     setShape(shape) {
-//         this.shape = shape
-  
-//     }
-//   }
 
 // array of questions that will be asked of the user
 const questions = [
@@ -59,34 +36,20 @@ async function init() {
         console.log(answers)
         if (inputShape === 'circle') {
             const generatedShape = new Circle(answers.shapeColor, answers.logo, answers.textColor)
-            // generatedShape.render()
             fs.writeFile('logo.svg', generatedShape.render(), () => {console.log('Generated logo.svg')})
-            // return generatedShape
         } else if (inputShape === 'square') {
             const generatedShape = new Square(answers.shapeColor, answers.logo, answers.textColor)
-            // generatedShape.render()
             fs.writeFile('logo.svg', generatedShape.render(), () => {console.log('Generated logo.svg')})
-            // return generatedShape
         } else {
             const generatedShape = new Triangle(answers.shapeColor, answers.logo, answers.textColor)
-            // generatedShape.render()
             fs.writeFile('logo.svg', generatedShape.render(), () => {console.log('Generated logo.svg')})
-            // return generatedShape
         }
-        // fs.writeFile('logo.svg', JSON.stringify(answers), () => {console.log('Generated logo.svg')})
-        // fs.writeFile('logo.svg', generatedShape.render(), () => {console.log('Generated logo.svg')})
     })
     .catch((err) => {
         console.error(err)
         throw(new Error('Unable to generate the svg logo, try again.', err))
     })
-    // return answers
 }
 
-// throw the new error or return it 
 // throw stops the program, return new error continues the logic
 init()
-
-
-
-//   module.exports(answers)
