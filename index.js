764144974ff6add9c51dd644dbee633b4fe78d7f
JA <69608172+ClassJay@users.noreply.github.com>
2024-07-09 13:37:29 -0500

    Refactored code

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const inquirer = require('inquirer')
 const fs = require('fs')
 const {Circle, Square, Triangle } = require('./lib/shape')
 
-
 // array of questions that will be asked of the user
 const questions = [
     {
@@ -11,7 +10,7 @@ const questions = [
         message: 'What 3 letters would you like your logo to have?',
         validate: (logoLetters) => {
             return logoLetters.length <= 3 || 'Must be 3 characters or less. Remove characters'
-        },
+        }
     },
     {
         type: 'input',
@@ -35,8 +34,7 @@ function init() {
     inquirer.prompt(questions)
     .then((answers) => {
         const inputShape = answers.shape 
-        // const shapeObject = JSON.parse(inputShape)
-        console.log(answers)
+
         if (inputShape === 'circle') {
             const generatedShape = new Circle(answers.shapeColor, answers.logo, answers.textColor)
             fs.writeFile('logo.svg', generatedShape.render(), () => {console.log('Generated logo.svg')})
@@ -54,5 +52,5 @@ function init() {
     })
 }
 
-// throw stops the program, return new error continues the logic
+
 init()
